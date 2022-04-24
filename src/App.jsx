5c983b428e9900858708a1a74305678327819112
Robertcego <rocerdasgo@gmail.com>
2022-04-23 23:34:53 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.css';
+import useFetch from './hooks/useFetch/useFetch';
+
+const url = 'https://jsonplaceholder.typicode.com/users';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isPending, isError } = useFetch(url);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <h1>Fetching data from API</h1>
+      {data &&
+        data.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <h3>Username: {user.username}</h3>
+            <p>
+              Address:{' '}
+              {`${user.address.street}, ${user.address.suite}, ${user.address.city}`}
+            </p>
+            <p>Phone: {user.phone}</p>
+            <p>Email: {user.email}</p>
+          </div>
+        ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -3,7 +3,32 @@ import { useState, useEffect } from 'react';
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [isPending, setIsPending] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const [isError, setIsError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Response not OK');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setIsError(false);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setIsError(error.message);
+      });
+  }, [url]);
+
+  return {
+    data,
+    isPending,
+    isError,
+  };
 };
 
 export default useFetch;
